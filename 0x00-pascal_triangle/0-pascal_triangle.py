b3727b1fa4c8d6d55a663b1eaad50115d9c4b830
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-10-03 23:15:35 +0300

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -3,37 +3,37 @@
 
 def pascal_triangle(n):
     """
-    Generating Pascal's Triangle up to n rows.
+    Generates Pascal's Triangle up to n rows.
     
     Args:
-        n (int): Number of rows to generate.
+        n (int): The number of rows to generate.
         
     Returns:
         list: A list of lists where each inner list represents a row in Pascal's Triangle.
     """
-    # Return an empty list if input is less than or equal to zero
+    # Return an empty list if the input is less than or equal to zero
     if n <= 0:
         return []
 
-    # Initializing outer list to hold rows of Pascal's Triangle
+    # Initialize the outer list to hold the rows of Pascal's Triangle
     pascal_t = []
 
-    # Looping through every row index 'r' to build Pascal's Triangle row by row
-    for r in range(n):
-        # Creating a new row and add the number '1' at the start of each row
+    # Loop through each row index 'i' to build Pascal's Triangle row by row
+    for i in range(n):
+        # Create a new row and add the number '1' at the start of each row
         pascal_t.append([])
-        pascal_t[r].append(1)
+        pascal_t[i].append(1)
 
-        # Filling the current row based on the sum of two numbers from the previous row
-        for m in range(1, r):
+        # Fill the current row based on the sum of two numbers from the previous row
+        for j in range(1, i):
             # Get the values from the previous row that need to be added
-            left_value = pascal_t[r-1][m-1]
-            right_value = pascal_t[r-1][m]
+            left_value = pascal_t[i-1][j-1]
+            right_value = pascal_t[i-1][j]
             # Append their sum to the current row
-            pascal_t[r].append(left_value + right_value)
+            pascal_t[i].append(left_value + right_value)
 
         # Add '1' at the end of the row for all rows except the first one
-        if r != 0:
-            pascal_t[r].append(1)
+        if i != 0:
+            pascal_t[i].append(1)
 
     return pascal_t
