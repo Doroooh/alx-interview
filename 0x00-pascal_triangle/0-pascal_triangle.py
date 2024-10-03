cc287c6afedd9ea22ceb356293129d015748d204
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-10-03 23:12:41 +0300

    Pascal's Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -1,25 +1,42 @@
 #!/usr/bin/python3
-"""Pascal Triangle Interview Challenge"""
+"""Pascal Triangle"""
 
+#!/usr/bin/python3
+"""A module for generating Pascal's Triangle up to a specified number of rows."""
 
 def pascal_triangle(n):
-    """returns a list of lists of numbers
-    representing the pascal triangle"""
+    """
+    Generating Pascal's Triangle up to n rows.
+    
+    Args:
+        n (int): Number of rows to generate.
+        
+    Returns:
+        list: A list of lists where each inner list represents a row in Pascal's Triangle.
+    """
+    # Return an empty list if input is less than or equal to zero
     if n <= 0:
         return []
 
+    # Initializing outer list to hold rows of Pascal's Triangle
     pascal_t = []
 
-    for i in range(n):
+    # Looping through every row index 'r' to build Pascal's Triangle row by row
+    for r in range(n):
+        # Creating a new row and add the number '1' at the start of each row
         pascal_t.append([])
-        pascal_t[i].append(1)
+        pascal_t[r].append(1)
 
-        for j in range(1, i):
-            x = pascal_t[i-1][j-1]
-            y = pascal_t[i-1][j]
-            pascal_t[i].append(x+y)
+        # Filling the current row based on the sum of two numbers from the previous row
+        for m in range(1, r):
+            # Get the values from the previous row that need to be added
+            left_value = pascal_t[r-1][m-1]
+            right_value = pascal_t[r-1][m]
+            # Append their sum to the current row
+            pascal_t[r].append(left_value + right_value)
 
-        if(n != 0 and i != 0):
-            pascal_t[i].append(1)
+        # Add '1' at the end of the row for all rows except the first one
+        if r != 0:
+            pascal_t[r].append(1)
 
     return pascal_t
