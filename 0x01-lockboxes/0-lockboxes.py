a505800ec86a5e66369db2701671a0247aca57ce
Waithira <50845913+Doroooh@users.noreply.github.com>
2024-10-10 21:40:30 +0300

    Lockboxes Task

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
 
 """
-Problem: You have n number of locked boxes in front of you.
-         Each box is numbered sequentially from 0 to n - 1
-         and each box may contain keys to the other boxes.
-Task: Writing method that will determine if all boxes can be opened.
+there are n number of locked boxes. Every box is numbered sequentially from 0 to n - 1, and every box contains keys to the other boxes.
+Assignement: Writing the method that determines if all boxes can be opened.
 """
 
 
@@ -19,26 +17,30 @@ def canUnlockAll(boxes):
     Returns:
     True if all boxes can be opened, False otherwise.
     """
-    # Check if input is a list and not empty
+    
+    # Checking if input is a list and ensuring it is non-empty
     if type(boxes) is not list:
-        return False
+        return False  # If not a list, immediately return False
     elif len(boxes) == 0:
-        return False
-
-    # Iterate through each box starting from the second box (index 1)
-    for k in range(1, len(boxes) - 1):
-        boxes_checked = False  # Flag to check if the current box k can be opened
-
-        # Iterate through all boxes to find if any box contains the key to box k
+        return False  # If list is empty, return False
+    
+    # Iterating over every box starting from index 1 (box 0 is always open)
+    for r in range(1, len(boxes) - 1):
+        boxes_checked = False  # Flagging for checking if current box 'r' can be opened
+        
+        # Looping through all boxes to find a key to box 'r'
         for idx in range(len(boxes)):
-            # Check if box k's key is in another box and it's not the same box
-            boxes_checked = k in boxes[idx] and k != idx
+            # Checking if current box contains the key to box 'r'
+            # Ensuring the box doesn't contain its own key (r != idx)
+            boxes_checked = r in boxes[idx] and r != idx
+            
+            # If a key to box 'r' is found, stop searching
             if boxes_checked:
-                break  # If box k can be opened, exit the inner loop
-
-        # If no box contains the key to box k, return False
+                break
+        
+        # If no key was found for box 'r', return False as it cannot be opened
         if not boxes_checked:
             return False
-
+    
     # If all boxes can be opened, return True
     return True
